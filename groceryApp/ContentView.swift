847af5594d950e54dcdcd1d235539cecb4e0921f
Andrei <scentistx@gmail.com>
2022-10-13 17:22:52 +0200

    Initial commit

---
+++ groceryApp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  groceryApp
+//
+//  Created by Andrei on 13.10.2022.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ groceryApp/groceryAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  groceryAppApp.swift
+//  groceryApp
+//
+//  Created by Andrei on 13.10.2022.
+//
+//
+
+import SwiftUI
+
+@main
+struct groceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
