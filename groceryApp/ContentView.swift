d39b4503592b486cc536e2a6501ba91d000d59e7
Andrei <scentistx@gmail.com>
2022-10-15 12:25:32 +0200

    Updating code

--- groceryApp/ContentView.swift
+++ groceryApp/ContentView.swift
@@ -8,20 +8,12 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+@main
+struct SwiftUI_GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
